eb09a908ad2ce374285aac54ebaf3086b7c69825
Cihan <bulutyusufcihan@gmail.com>
2024-12-17 22:56:08 +0300

    add score

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+
 
 public class Ball : MonoBehaviour
 {
-    public float jumpForce = 5f;   // Yukarý doðru zýplama kuvveti
-    public float horizontalForce = 3f; // Saða veya sola uygulanan kuvvet
-    private Rigidbody rb; 
+    public float jumpForce = 5f;   
+    public float horizontalForce = 3f;
+    private Rigidbody rb;
+    public int score = 0;
+    public TMP_Text scoreText;
 
+    private bool isDead = false;
     void Start()
     {
-       
+
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // Ekrana dokunulduðunda zýpla ve yön belirle
-        if (Input.GetMouseButtonDown(0)) // 0: Sol fare butonu veya mobil ekran dokunmasý
+        
+        if (Input.GetMouseButtonDown(0)) 
         {
             JumpBasedOnTouch();
         }
@@ -31,15 +38,37 @@ public class Ball : MonoBehaviour
 
         rb.velocity = new Vector3(0, jumpForce, 0);
 
-     
+
         if (touchPosition.x < screenMiddle)
         {
             rb.velocity += Vector3.left * horizontalForce;
         }
-      
+
         else if (touchPosition.x > screenMiddle)
         {
             rb.velocity += Vector3.right * horizontalForce;
+        }
+    }
+
+
+    // Trigger'dan geçildiðinde çalýþýr
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hoop")) 
+        {
+            score += 1; 
+            UpdateScoreUI(); 
+
+            Debug.Log("Skor: " + score);
+        }
+    }
+
+    // Skoru ekrana yazdýran fonksiyon
+    void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = " " + score;
         }
     }
 }
